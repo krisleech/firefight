954ad78b5b4c1a66107906928782afdf1e19d11a
Kris Leech <kleech@housetrip.com>
2014-07-02 17:57:44 +0100

    feature spec for adding a person

---
+++ spec/features/adding_person_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature 'adding person' do
+
+  scenario 'adding multiple people' do
+    visit diary_path
+
+    within('#add_person') do
+      fill_in 'First name', with: 'Kris'
+      fill_in 'Last name', with: 'Leech'
+    end
+
+    click_button 'Save'
+
+    expect(page).to have_content 'Person Saved.' # page contains json response!?!
+    expect(page).to have_content 'Kris Leech'
+  end
+end
+
